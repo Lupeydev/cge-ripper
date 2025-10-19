8705a2914fde94a810a2c2a4f0fad024754e4312
Lupey <monkerunkeVR@gmail.com>
2025-10-19 17:10:49 -0500

    cge-ripper c#

---
+++ directmap.py
@@ -0,0 +1,14 @@
+import requests
+
+base_url = "https://wavespray.dathost.net/fastdl/teamfortress2/679d9656b8573d37aa848d60/maps/"
+map_name = "demo"
+
+url = f"{base_url}{map_name}.bsp"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(f"✅ Found: {map_name}")
+    with open(f"{map_name}.bsp", "wb") as f:
+        f.write(response.content)
+else:
+    print(f"❌ Not found: {map_name}")

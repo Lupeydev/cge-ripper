59290229e11c6cd5bd7d46d3a97cd803ee70070c
Lupey <monkerunkeVR@gmail.com>
2026-03-29 00:35:10 -0500

    made it input instead

--- directmap.py
+++ directmap.py
@@ -1,7 +1,7 @@
 import requests
 
 base_url = "https://wavespray.dathost.net/fastdl/teamfortress2/679d9656b8573d37aa848d60/maps/"
-map_name = "ord_say"
+map_name = input("Map Name: ")
 
 url = f"{base_url}{map_name}.bsp"
 response = requests.get(url)
